12ef8482f49d5f3a7232df59812be2e2f56623d4
Ilia Ryabukhin <Ilia.Ryabukhin@studentpartner.com>
2020-07-09 11:42:08 +0300

    app added

---
+++ SampleForms/SampleForms/Human.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleForms
+{
+    class Human
+    {
+        public string Name { get; private set; }
+        private string surname;
+        private int age;
+
+
+        public Human(string name = "", string surname = "", int age = 0)
+        {
+            this.Name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        /*
+        string getName()
+        {
+            return name;
+        }
+        void setName(string value)
+        {
+            name = value;
+        }
+        */
+        /*
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        */
+    }
+}
